68fff2813709fa1f9a154196a9aaa15c3524e3ce
hibbardsclass <142955584+hibbardsclass@users.noreply.github.com>
2025-01-24 11:28:26 -0700

    Create setup.py
    adding the ability to install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="taiga-contrib-google-auth",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    description="Google OAuth integration for Taiga",
+    author="Original Author",
+    url="https://github.com/donbowman/taiga-contrib-google-auth",
+)
